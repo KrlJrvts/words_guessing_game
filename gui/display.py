2e06e947723bdb9bbf69185b7bba9ca0b348a221
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-06 23:00:58 +0200

    alpha 0.02 build - GUI labels added

--- gui/display.py
+++ gui/display.py
@@ -4,18 +4,26 @@ from tkinter import *
 window = Tk()
 window.title("Words guessing game")
 window.geometry("900x500")
-window.configure(bd=5, background="green")
+window.configure(bd=20, background="green")
+
+# Main label
+label1 = Label(window, text="Welcome to the word guessing game!", font=("Helvetica", 16))
+label1.config(height=1, width=50)
+label1.place(x=150, y=10)
+label2 = Label(window, text="Please choose from options below", font=("Helvetica", 16))
+label2.config(height=1, width=50)
+label2.place(x=150, y=40)
+
 
 # Buttons for main menu
-button1 = Button(window, text="New game", fg="black")
+button1 = Button(window, text="New game", fg="black", font="Helvetica")
 button1.config(height=1, width=20)
-button1.place(x=350, y=100)
-button2 = Button(window, text="Scoreboard", fg="black")
+button1.place(x=350, y=200)
+button2 = Button(window, text="Scoreboard", fg="black", font="Helvetica")
 button2.config(height=1, width=20)
-button2.place(x=350, y=150)
-button3 = Button(window, text="Exit game", fg="black")
+button2.place(x=350, y=250)
+button3 = Button(window, text="Exit game", fg="black", font="Helvetica")
 button3.config(height=1, width=20)
-button3.place(x=350, y=200)
-
+button3.place(x=350, y=300)
 
 window.mainloop()
