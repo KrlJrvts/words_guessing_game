d1d868ca0dfe18a62b3c10eb5ce238e817398a84
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-06 22:17:36 +0200

    alpha 0.01 build - GUI build start

---
+++ gui/display.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Word guessing game")
+root.geometry("900x500")
+root.configure(bd=10, background="white")
+
+my_label = Label(root, text="Hello World")
+
