cf3ede2da17bc3a36aab2098d6b9994da513a65b
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2022-11-14 21:33:12 +0200

    alpha 0.08 - code split based on frames

--- gui/design.py
+++ gui/design.py
@@ -10,18 +10,3 @@ class Ft:
     h2i = "Helvetica", -16, "italic"
     txt = "Helvetica", -12
     txtb = "Helvetica", -12, "bold"
-
-
-# class Fg:
-#     dbl = "#1e698d"
-#     blck = "#131925"
-#
-#
-# class Bg:
-#     lbl = "#c2d5db"
-#     lgrn = "#8abca7"
-#     bl = "#789fbb"
-#     dbl = "#1e698d"
-#     blck = "#131925"
-#     # gry = "gary"
-#     # grn = "green"
